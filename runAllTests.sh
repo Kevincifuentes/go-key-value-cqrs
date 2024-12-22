8ca257fbe95777038f15f92577191d988c87903a
Kevin <16136308+Kevincifuentes@users.noreply.github.com>
2024-12-22 10:02:05 +0100

    [Get-keys] Excluding generated files from coverage run
    Signed-off-by: Kevin <16136308+Kevincifuentes@users.noreply.github.com>

--- runAllTests.sh
+++ runAllTests.sh
@@ -9,7 +9,7 @@ for mod in $mods; do
             coverprofile=$assets_path/out.txt
             echo "Processing module $mod"
             (cd "$mod"; go test ./... -coverpkg=go-key-value-cqrs/... -coverprofile=$coverprofile)
-            (cat $coverprofile | grep -v "mode: set" >> "$assets_path"/final.out)
+            (cat $coverprofile | grep -v ".gen.go" | grep -v "mode: set" >> "$assets_path"/final.out)
             rm $coverprofile
 done
 go tool cover -html=$final_coverage_file -o "$assets_path"/coverage.html
