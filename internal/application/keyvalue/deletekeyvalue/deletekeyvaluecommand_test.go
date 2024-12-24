f0c4f20bb8558b4a64ad4dc2a44e3fec3047964f
Kevin <16136308+Kevincifuentes@users.noreply.github.com>
2024-12-24 10:17:04 +0100

    [Delete-key] Added DeleteKey Command/CommandHandler with tests
    Signed-off-by: Kevin <16136308+Kevincifuentes@users.noreply.github.com>

---
+++ internal/application/keyvalue/deletekeyvalue/deletekeyvaluecommand_test.go
@@ -0,0 +1,63 @@
+package deletekeyvalue
+
+import (
+	"errors"
+	"github.com/jaswdr/faker/v2"
+	"github.com/labstack/gommon/log"
+	"go-key-value-cqrs/application/cqrs/commandbus"
+	"go-key-value-cqrs/domain"
+	"go-key-value-cqrs/objectmothers"
+	"testing"
+)
+
+type testInterfaceMock struct {
+	numberOfCalls int
+	expectedKey   string
+	expectedError error
+}
+
+func (mock *testInterfaceMock) Add(_ domain.KeyValue) error {
+	return nil
+}
+
+func (mock *testInterfaceMock) Delete(key string) error {
+	mock.numberOfCalls++
+	mock.expectedKey = key
+	return mock.expectedError
+}
+
+var mock testInterfaceMock
+var fakerInstance = faker.New()
+var keyValueObjectMother = objectmothers.KeyValueObjectMother{FakerInstance: &fakerInstance}
+
+func registerCommand(expectedError error) {
+	mock = testInterfaceMock{numberOfCalls: 0, expectedKey: "", expectedError: expectedError}
+	handler := CommandHandler{&mock}
+	err := commandbus.Load(handler)
+	if err != nil {
+		log.Warnf("Error loading command handler %v. Error=%v", handler, err)
+	}
+}
+
+func TestDeleteKeyValueCommandResolvedCorrectly(t *testing.T) {
+	// given
+	expectedKeyValue := keyValueObjectMother.CreateRandom()
+	expectedKey := expectedKeyValue.Key.Key
+	expectedErr := errors.New("any")
+	registerCommand(expectedErr)
+	command := Command{Key: expectedKey}
+
+	// when
+	err := commandbus.Execute(command)
+
+	// then
+	if err == nil || !errors.Is(err, expectedErr) {
+		t.Errorf("Test failed! Expected to be successful call for %v command. Actual: Error=%v",
+			command, err)
+	}
+	expectedNumberOfCalls := 1
+	if mock.numberOfCalls != expectedNumberOfCalls {
+		t.Errorf("Expected number of calls not meet on testInterfaceMock. Expected=%v Actual=%v",
+			expectedNumberOfCalls, mock.numberOfCalls)
+	}
+}
