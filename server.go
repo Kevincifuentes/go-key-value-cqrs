76ab9112ee2889cb209f581cbc483588c49d12e8
Kevin <16136308+Kevincifuentes@users.noreply.github.com>
2024-12-27 11:37:01 +0100

    [Env-file] Env variables PORT and OPENAPI path to env file
    Signed-off-by: Kevin <16136308+Kevincifuentes@users.noreply.github.com>

--- server.go
+++ server.go
@@ -1,22 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"go-key-value-cqrs/infrastructure/api"
 	"go-key-value-cqrs/infrastructure/api/metrics"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
 )
 
-const OpenAPISpecRelativePath = "./api/keyvalue/api.yml"
+const defaultOpenAPISpecRelativePath = "./api/keyvalue/api.yml"
 
 func main() {
-	handler := api.InitHandler(OpenAPISpecRelativePath)
+	loadEnvFile()
 
+	openApiPath := retrieveOsVariableOrDefault("OPENAPI_RELATIVE_PATH", defaultOpenAPISpecRelativePath)
+	handler := api.InitHandler(openApiPath)
+
+	port := retrieveOsVariableOrDefault("PORT", "8080")
 	server := &http.Server{
 		Handler: metrics.Metrics(handler),
-		Addr:    "0.0.0.0:8080",
+		Addr:    fmt.Sprintf("0.0.0.0:%v", port),
 	}
-	log.Println("Starting server on port 8080")
+	log.Printf("Starting server on port %v\n", port)
 	log.Println(server.ListenAndServe())
+}
+
+func retrieveOsVariableOrDefault(environmentVariableName string, defaultValue string) string {
+	relativePath, isPresent := os.LookupEnv(environmentVariableName)
+	if !isPresent {
+		return defaultValue
+	}
+	return relativePath
+}
+
+func loadEnvFile() {
+	err := godotenv.Load(".env.test.local")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
